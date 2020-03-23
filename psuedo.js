01d6ba07cbc97bc5e5c5cfa2283d9c3cb1e83187
unknown <lfreeman0820@gmail.com>
2020-03-23 14:07:27 -0700

    updated

---
+++ psuedo.js
@@ -0,0 +1,35 @@
+// things needed
+// user input
+// random password based on input
+// certain criteria:
+// ask for length of pw at least 8 
+// lowercase
+// uppercase
+// special characters
+​
+​
+// functionality:
+// click button
+// add event listener to click button that runs function to show first prompt
+​
+// show series of prompts
+// each prompt ask password question then store user input(yes or no)
+// first prompt ask user for length
+// show prompt grabbing length and store in variable
+// var length = user input
+​
+// second prompt ask user for lowercase
+// if first prompt answer true show second prompt
+// then store prompt data in variable
+​
+// third case ask for upper
+// if second prompt true show third
+// then store prompt data in variable
+​
+// fourth ask fo special characters
+// if third prompt true show fourth
+// then store prompt data in variable
+​
+// make sure to validate 
+// pw needs to be at least one from each prompt and meet pw length
+// show pw in alert or page
--- script.js
+++ script.js
@@ -1,3 +1,84 @@
+var numberCharacters = [ "0", "1", "2", "3", "4", "5", "6", "7", "8"];
+var upperCaseCharacters = [ "A", "B", "C", "3", "4", "5", "6", "7", "8"];
+var lowerCaseCharacters = [ "a", "b", "c", "3", "4", "5", "6", "7", "8"];
+var specialCharacters = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[',
+'~','-','_','.'];
+// Fill in the rest of the ABCs and abcs above
+
+
+function passwordChoices () {
+  var length = parseInt (prompt ("How long would you like the password to be?"));
+  if (length <8) { (alert ("Password needs to be at least 8 characters long")) 
+  
+  }
+  if (length >128) { (alert ("Password needs to be less than 128 characters long")) 
+  
+}
+   var gotNumberCharacters = confirm ("Do you want number characters?");
+   var gotUpperCaseCharacters = confirm ("Do you want upper case characters?");
+   var gotLowerCaseCharacters = confirm ("Do you want lower case characters?");
+   var gotSpecialCharacters = confirm ("Do you want special characters?");
+
+  if (gotNumberCharacters === false &&
+    gotUpperCaseCharacters === false &&
+    gotLowerCaseCharacters === false &&
+    gotSpecialCharacters === false) {
+      alert ("Password needs to be less than 128 characters long")
+      return;
+    }
+
+  var passwordOptions = {
+    length: length,
+    gotNumberCharacters: gotNumberCharacters,
+    gotUpperCaseCharacters: gotUpperCaseCharacters,
+    gotLowerCaseCharacters: gotLowerCaseCharacters,
+    gotSpecialCharacters: gotSpecialCharacters
+    
+  }  
+ return passwordOptions
+
+
+}
+
+function myRandom(array) {
+  var randomIndex = Math.floor(Math.random() * array.length);
+  var randomElement = array[randomIndex]
+  return randomElement
+
+}
+
+
+function generatePassword() {
+var selectedChoices = passwordChoices ()
+var result = []
+var store1Character = []
+var store2Character = []
+if (selectedChoices.gotNumberCharacters) {
+  store1Character = store1Character.concat(gotNumberCharacters);
+  store2Character.push(myRandom(gotNumberCharacters))
+  
+}
+// do if statements one for uppercase, lowercase and special above!
+
+for (i = 0; i < selectedChoices.length; i++) {
+  var store1Character = myRandom(store1Character)
+  result.push(store1Character)
+}
+
+for (i = 0; i < store2Character.length; i++) {
+  result[i] = store2Character[i];
+}
+return result.join("")
+
+}
+
+
+
+
+
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -12,3 +93,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
